b53f7d490464fa57cc60aeb973af0da97213a939
nishidy <nishidyatutokyo@gmail.com>
2015-02-22 21:16:17 +0900

    Employed akks.actor to separately deal with parsing and creating bag-of-words in bowActor.

--- Build.sbt
+++ Build.sbt
@@ -8,3 +8,8 @@ scalaVersion := "2.10.3"
 libraryDependencies += "com.github.scopt" %% "scopt" % "3.3.0"
 
 resolvers += Resolver.sonatypeRepo("public")
+
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.1"
--- src/main/scala/ParseWikipediaXML.scala
+++ src/main/scala/ParseWikipediaXML.scala
@@ -1,6 +1,9 @@
 import scala.io.Source
 import scala.xml.XML
 import scala.language.postfixOps
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
 
 object test{
 
@@ -10,6 +13,40 @@ object test{
 		oftitle: String = ""
 	)
 
+	class bowActor extends Actor {
+		def receive = {
+			case text:String =>
+
+				// allwords is list
+				// exclude stopwords and words that include symbols
+				val allwords = text.split( Array(' ',',','.') ).filter( x=>x.size>1 && (!stopword(x)) && allAlnum(x) )
+
+				// onewords is list in which redundant words are excluded
+				val onewords = allwords.toSet.toList
+
+				val zipped = for( word <- onewords ) yield List( word, allwords.filter( _ == word ).size.toString )
+
+				if(zipped.size>0) println(zipped.flatten.mkString(" "))
+				else ()
+
+			case _ =>
+
+		}
+
+		def stopword(x:String): Boolean = {
+			val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(',')
+			stopwords.contains(x)
+		}
+
+		def allAlnum(x:String): Boolean = {
+			val reg = """^([a-z0-9-]+)$""".r
+			x match{
+				case reg(_) => true
+				case _ => false
+			}
+		}
+
+	}
 
 	def main(args: Array[String]) = {
 
@@ -29,6 +66,9 @@ object test{
 			help("help") text("print this usage text.")
 		}
 
+		val system = ActorSystem("system")
+		val actor = system.actorOf(Props[bowActor],"bow")
+
 		parser.parse(args, Args()) match {
 			case Some(c) => {
 				val s = Source.fromFile(c.ifwiki)
@@ -44,20 +84,8 @@ object test{
 
 							for{ node <- XML.loadString( page.toString ) \ "revision" \ "text"
 								 text = node text }{
-
-								// allwords is list
-								// exclude stopwords and words that include symbols
-								val allwords = text.split( Array(' ',',','.') ).filter( x=>x.size>1 && (!stopword(x)) && allAlnum(x) )
-
-								// onewords is list in which redundant words are excluded
-								val onewords = allwords.toSet.toList
-
-								val zipped = for( word <- onewords ) yield List( word, allwords.filter( _ == word ).size.toString )
-
-								if(zipped.size>0) println(zipped.flatten.mkString(" "))
-								else ()
+								actor ! text
 							}
-
 							page.clear
 						}
 
@@ -70,19 +98,6 @@ object test{
 			case None => {
 				println("Failed to parse.")
 				sys.exit(1)
-			}
-		}
-
-		def stopword(x:String): Boolean = {
-			val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(',')
-			stopwords.contains(x)
-		}
-
-		def allAlnum(x:String): Boolean = {
-			val reg = """^([a-z0-9-]+)$""".r
-			x match{
-				case reg(_) => true
-				case _ => false
 			}
 		}
 
