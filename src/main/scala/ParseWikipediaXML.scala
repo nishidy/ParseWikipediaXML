cc09e03b581089f2cf0a1c5183cf71b54d70d7f4
nishidy <nishidyatutokyo@gmail.com>
2015-03-04 22:12:51 +0900

    Corrected the definition of 'm' and 'x'. They are related to the number of words in a page. (Scala code)

--- src/main/scala/ParseWikipediaXML.scala
+++ src/main/scala/ParseWikipediaXML.scala
@@ -28,32 +28,35 @@ object test{
 				// allwords is list
 				// exclude stopwords and words that include symbols
 				val allwords =
-					text.split( Array(' ',',','.') ).
-					filter( x => x.size>=minl && x.size<=maxl && (!stopword(x)) && allAlnum(x) ).
-					map( x => mapDict.get(x) match { case None =>  x; case Some(v) => v } )
+					text.split( Array(' ',',','.') )
+					.filter( x => (!stopword(x)) && allAlnum(x) )
+					.map( x => mapDict.get(x) match { case None =>  x; case Some(v) => v } )
 
 				// onewords is list in which redundant words are excluded
 				val onewords = allwords.toSet.toList
 
-				val zipped =
-					for( word <- onewords )
-						yield ( word, allwords.filter( _==word ).size.toString )
+				if( allwords.size > maxl || allwords.size < minl ) ()
+				else {
+					val zipped =
+						for( word <- onewords )
+							yield ( word, allwords.filter( _==word ).size.toString )
 
-				if( zipped.size>0 )
-					synchronized {
-						writer.write( zipped.filter( _._2.toInt>=minc ).
-						map( x => List(x._1,x._2) ).
-						flatten.mkString(""," ","\n") )
-					}
-				else ()
+					if( zipped.size>0 )
+						synchronized {
+							writer.write( zipped.filter( _._2.toInt>=minc )
+							.map( x => List(x._1,x._2) )
+							.flatten.mkString(""," ","\n") )
+						}
+					else ()
+				}
 
 			case _ =>
 
 		}
 
 		def stopword(x:String): Boolean = {
-			val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(',')
-			stopwords.contains(x)
+			val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+			stopwords.split(',').contains(x)
 		}
 
 		def allAlnum(x:String): Boolean = {
@@ -89,15 +92,15 @@ object test{
 
 			opt[Int]('m',"min-word-length") action { (x,c) =>
 				c.copy(minl = x.toInt)
-			} text ("Minimum word length")
+			} text ("Minimum number of words that a page should have")
 
 			opt[Int]('x',"max-word-length") action { (x,c) =>
 				c.copy(maxl = x.toInt)
-			} text ("Maximum word length")
+			} text ("Maximum number of words that a page should have")
 
 			opt[Int]('c',"min-word-count") action { (x,c) =>
 				c.copy(minc = x.toInt)
-			} text ("Minimum word count")
+			} text ("Minimum number that a word should have")
 
 			help("help") text("print this usage text.")
 		}
