f570b1a865d20a4f8b829a70843a2ab83a994353
nishidy <nishidyatutokyo@gmail.com>
2015-03-04 22:40:06 +0900

    Corrected the definition of 'm' and 'x'. They are related to the number of words in a page. (Go code)

--- ParseWikipediaXML.go
+++ ParseWikipediaXML.go
@@ -18,7 +18,7 @@ import (
 
 var stopwords string = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
 
-func make_map(text string, minl, maxl int, dict map[string]string, stop []string) map[string]int {
+func make_map(text string, dict map[string]string, stop []string) (int, map[string]int) {
 
 	var re *regexp.Regexp
 
@@ -29,11 +29,12 @@ func make_map(text string, minl, maxl int, dict map[string]string, stop []string
 	re, _ = regexp.Compile("^[0-9a-z][-|0-9a-z]+$")
 	words := strings.Split(text, " ")
 
+	var wc = 0
 	for _, word := range words {
 		word = strings.ToLower(word)
 		if re.MatchString(word) {
 
-			if len(word) < minl || len(word) > maxl {
+			if len(word) == 1 {
 				continue
 			}
 
@@ -57,9 +58,11 @@ func make_map(text string, minl, maxl int, dict map[string]string, stop []string
 			} else {
 				words_cnt[word] = 1
 			}
+
+			wc++
 		}
 	}
-	return words_cnt
+	return wc, words_cnt
 }
 
 func parse(str, regstr string) string {
@@ -134,9 +137,9 @@ func main() {
 	var ifdict = flag.String("d", "", "Input File(dictionary)")
 	var ofcont = flag.String("s", "", "Output File(Contents)")
 	var oftitle = flag.String("t", "", "Output File(Title)")
-	var minl = flag.Int("m", 2, "min_word_length")
-	var maxl = flag.Int("x", 256, "max_word_length")
-	var minc = flag.Int("c", 2, "min_word_count")
+	var minl = flag.Int("m", 1, "Minimum number of words that a page should have")
+	var maxl = flag.Int("x", 65535, "Maximum number of words that a page should have")
+	var minc = flag.Int("c", 2, "Minimum number that a word should have")
 	var cat = flag.String("g", ".*", "Category(regular expression)")
 	var json = flag.Bool("j", false, "Generate bug-of-words in JSON format")
 
@@ -248,7 +251,10 @@ func main() {
 				}
 
 				var text_map = make(map[string]int)
-				text_map = make_map(text, *minl, *maxl, dict, stop)
+				var wc = 0
+				if wc, text_map = make_map(text, dict, stop); wc < *minl || wc > *maxl {
+					continue
+				}
 
 				/* Only one thread can write the result into file at once */
 				cf <- 1
