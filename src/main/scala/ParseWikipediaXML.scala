9dc604c89d93695f43551d43b6386cfde055b6b0
nishidy <nishidyatutokyo@gmail.com>
2015-03-07 00:16:39 +0900

    I should have Listed stopwords avoiding the split of them many times. (Scala)

--- src/main/scala/ParseWikipediaXML.scala
+++ src/main/scala/ParseWikipediaXML.scala
@@ -27,14 +27,18 @@ object test{
 	case class Text(text:String, mapDict:Map[String,String], minl:Int, maxl:Int, minc:Int, writer:StreamWriter)
 
 	class bowActor extends Actor {
+
+		val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(",")
+
 		def receive = {
+
 			case Text(text,mapDict,minl,maxl,minc,writer) =>
 
 				// allwords is list
 				// exclude stopwords and words that include symbols
 				val allwords =
 					text.split( Array(' ',',','.') )
-					.filter( x => x.size>1 && (!stopword(x)) && allAlnum(x) )
+					.filter( x => x.size>1 && (!stopwords.contains(x)) && allAlnum(x) )
 					.map( x => mapDict.get(x) match { case None =>  x; case Some(v) => v } )
 
 				// onewords is list in which redundant words are excluded
@@ -59,11 +63,6 @@ object test{
 
 			case _ =>
 
-		}
-
-		def stopword(x:String): Boolean = {
-			val stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
-			stopwords.split(',').contains(x)
 		}
 
 		def allAlnum(x:String): Boolean = {
