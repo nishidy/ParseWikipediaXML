63abe38ee3629bb28b7260e27705ac0ee2ccc691
nishidy <nishidyatutokyo@gmail.com>
2015-08-12 01:41:43 +0900

    N-gram supported and avoid using a process map for the arguments list. ToDo: Remove dependency on a process map for words and counts in each process because it has side effect.

--- parseWikipediaXML.erl
+++ parseWikipediaXML.erl
@@ -1,48 +1,50 @@
 -module(parseWikipediaXML). 
 -export([main/0]). 
--import(string,[tokens/2,to_lower/1,to_integer/1]). 
--import(lists,[concat/1,map/2,filter/2,nth/2,any/2]). 
+-import(string,[tokens/2,to_lower/1,to_integer/1,join/2]). 
+-import(lists,[sublist/3,concat/1,map/2,filter/2,nth/2,any/2,reverse/1,keyfind/3,keydelete/3]). 
 
 main() ->
-	take_args(init:get_arguments()),
+	Args=take_args(init:get_arguments(),[{c,1},{n,1}]),
 	%io:format("~p.~n",[init:get_arguments()]),
-	%io:format("~p.~n",[get()]),
+	%io:format("~p.~n",[Args]),
 
-	case get(o) of
-		undefined -> error(badarg);
-		Fs ->
-			case file:open(Fs,write) of
-				{ok,Fho} -> put(fho,Fho);
-				{error,Reaso} ->io:format("~p.~n",[Reaso])
-			end
-	end,
-
-	case get(i) of
-		undefined -> error(badarg);
-		Fi ->
-			case file:open(Fi,read) of
-				{ok,Fhi} -> put(fhi,Fhi);
-				{error,Reason} ->io:format("~p.~n",[Reason])
-			end
-	end,
-
-	case get(fhi) of
-		undefined -> error(badarg);
-		File ->
-			line_concat(File,"")
+	case keyfind(fhi,1,Args) of
+		false -> error(badarg);
+		{fhi,Fhi} ->
+			line_concat(Args,Fhi,"")
 	end. 
 
-take_args([{c,[V|_]}|T]) ->
+take_args([{K,[V|_]}|T],Args) when
+	K=:=c; K=:=n ->
+	NewArgs =
+		case keyfind(K,1,Args) of
+			false -> Args;
+			_ -> keydelete(K,1,Args)
+		end,
 	{I,_} = to_integer(V),
-	put(c,I),
-	take_args(T);
-take_args([{K,[V|_]}|T]) ->
-	put(K,V),
-	take_args(T);
-take_args([{K,[]}|T]) ->
-	put(K,[]),
-	take_args(T);
-take_args([]) -> true. 
+	take_args(T,[{K,I}|NewArgs]);
+take_args([{K,[V|_]}|T],Args) when
+	K=:=o ->
+	case file:open(V,write) of
+		{ok,Fho} ->
+			take_args(T,[{K,V},{fho,Fho}|Args]);
+		{error,FsReason} ->
+			io:format("~p.~n",[FsReason])
+	end;
+take_args([{K,[V|_]}|T],Args) when
+	K=:=i ->
+	case file:open(V,read) of
+		{ok,Fhi} ->
+			take_args(T,[{K,V},{fhi,Fhi}|Args]);
+		{error,FiReason} ->
+			io:format("~p.~n",[FiReason])
+	end;
+take_args([{K,[V|_]}|T],Args) ->
+	take_args(T,[{K,V}|Args]);
+take_args([{K,[]}|T],Args) ->
+	take_args(T,[{K,""}|Args]);
+take_args([],Args) ->
+	Args. 
 
 if_stopwords(Word) ->
 	Stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your",
@@ -54,6 +56,21 @@ chomp([H|T]) ->
 		[] -> [];
 		_ -> [H | chomp(T)]
 	end. 
+
+ngram_count([H|T],G,N) when length(G)<N ->
+	ngram_count(T,[H|G],N); 
+ngram_count([H|T],G,N) ->
+	%io:format("~p~n",[G]),
+	M=join(reverse(G),":"),
+	case get(M) of
+		undefined -> put(M,1);
+		C -> put(M,C+1)
+	end,
+	case T of
+		[] -> "";
+		_  -> ngram_count(T,[H|sublist(G,1,length(G)-1)],N)
+	end;
+ngram_count([],_,_) -> "". 
 
 word_count([H|T]) ->
 	%%io:format("~p~p~n",[H,T]),
@@ -94,7 +111,7 @@ disp_processmap([{K,V}|T],Fho,C) ->
 disp_processmap([],Fho,_) ->
 	io:format(Fho,"~n",[]). 
 
-line_concat(File,Line) ->
+line_concat(Args,File,Line) ->
 	%io:format("Line=~p.~n",[Line]),
 
 	% Regular expression '.' does not match with \n.
@@ -107,7 +124,7 @@ line_concat(File,Line) ->
 
 			case file:read_line(File) of
 				{ok,Data} ->
-					line_concat(File,concat([Line,chomp(Data),' ']));
+					line_concat(Args,File,concat([Line,chomp(Data),' ']));
 				eof -> 
 					io:format("Finish reading file.~n");
 				{error,Reason} -> 
@@ -121,35 +138,49 @@ line_concat(File,Line) ->
 			% However, sublist takes index starting from 1
 
 			{Idx,Num} = nth(1,Match),
-			%io:format("~p~n",[tokens(lists:sublist(Line,Idx+1,Num),"\n,. ")]),
+			%io:format("~p~n",[tokens(sublist(Line,Idx+1,Num),"\n,. ")]),
 
-			case get(u) of
+			{fho,Fho} = keyfind(fho,1,Args),
+			{c,C} = keyfind(c,1,Args),
+			{n,N} = keyfind(n,1,Args),
 
-				undefined ->
+			case keyfind(u,1,Args) of
 
-					word_count(
-						filter(
-							fun(X) -> tokenizer(X) andalso not if_stopwords(X) end,
-							map(fun(X)->to_lower(X)end,tokens(lists:sublist(Line,Idx+1,Num),"\n,. "))
-						)
-					),
-					disp_processmap(get(),get(fho),get(c)),
-					erase(),
-					take_args(init:get_arguments());
+				false ->
+
+					case N of
+						1 ->
+							word_count(
+								filter(
+									fun(X) -> tokenizer(X) andalso not if_stopwords(X) end,
+									map(fun(X)->to_lower(X)end,tokens(sublist(Line,Idx+1,Num),"\n,. "))
+								)
+							);
+
+						N ->
+							ngram_count(
+								filter(
+									fun(X) -> tokenizer(X) andalso not if_stopwords(X) end,
+									map(fun(X)->to_lower(X)end,tokens(sublist(Line,Idx+1,Num),"\n,. "))
+								),
+								[],
+								N
+							)
+					end,
+
+					disp_processmap(get(),Fho,C),
+					erase();
 
 				_ ->
 					Wordlist=
 						filter(
 							fun(X) -> tokenizer(X) andalso not if_stopwords(X) end,
-							map(fun(X)->to_lower(X)end,tokens(lists:sublist(Line,Idx+1,Num),"\n,. "))
+							map(fun(X)->to_lower(X)end,tokens(sublist(Line,Idx+1,Num),"\n,. "))
 						),
-					Fho = get(fho),
-					C = get(c),
 					spawn(
 						fun() ->
 							word_count(Wordlist),
-							disp_processmap(get(),Fho,C),
-							erase()
+							disp_processmap(get(),Fho,C)
 						end
 					)
 
@@ -157,7 +188,7 @@ line_concat(File,Line) ->
 
 			case file:read_line(File) of
 				{ok,Data} ->
-					line_concat(File,concat([chomp(Data)," "]));
+					line_concat(Args,File,concat([chomp(Data)," "]));
 				eof -> 
 					io:format("Finish reading file.~n");
 				{error,Reason} -> 
