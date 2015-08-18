f368c05ebce6e489dd0ad72e51d19a80c9628f83
nishidy <nishidyatutokyo@gmail.com>
2015-08-18 23:02:42 +0900

    Overhaul the structure of code before applying Japanese.

--- java/ParseWikipediaXML.java
+++ java/ParseWikipediaXML.java
@@ -14,74 +14,203 @@ import java.lang.Runtime;
 import org.atilika.kuromoji.Token;
 import org.atilika.kuromoji.Tokenizer;
 
-class BOW implements Runnable {
+class ArgStore {
 
-	static Map<String,String> mapDict = new HashMap<String,String>();
-	static BufferedWriter bw;
-	static List<String> stopwords = new ArrayList<>();
-	private final static Object lock = new Object();
+	String ifwiki;
+	String ifdict;
+	String ofcont;
+	String oftitle;
+	String recateg;
+	int minl;
+	int maxl;
+	int minc;
+	boolean ifjap;
+	boolean ifverb;
 
-	String pageStr;
-	String categoryRe;
-	Integer minl = 2;
-	Integer maxl = 64;
-	Integer minc = 1;
+	ArgStore(CommandLine cl) throws ParseException {
 
-	public BOW(String pageStr, String categoryRe, Integer... margs){
-		this.pageStr = pageStr;
-		this.categoryRe = categoryRe;
-		this.minl = margs[0];
-		this.maxl = margs[1];
-		this.minc = margs[2];
+		if(cl.hasOption("i")) ifwiki = cl.getOptionValue("i");
+		else throw new ParseException("i is not specified.");
+
+		if(cl.hasOption("d")) ifdict = cl.getOptionValue("d");
+		else throw new ParseException("d is not specified.");
+
+		if(cl.hasOption("s")) ofcont = cl.getOptionValue("s");
+		else throw new ParseException("s is not specified.");
+
+		if(cl.hasOption("t")) oftitle= cl.getOptionValue("t");
+		//else throw new ParseException("t is not specified.");
+
+		if(cl.hasOption("m")) minl = Integer.parseInt(cl.getOptionValue("m"));
+		else minl=2;
+
+		if(cl.hasOption("x")) maxl = Integer.parseInt(cl.getOptionValue("x"));
+		else maxl=64;
+
+		if(cl.hasOption("c")) minc = Integer.parseInt(cl.getOptionValue("c"));
+		else minc=1;
+
+		if(cl.hasOption("g")) recateg = cl.getOptionValue("g");
+		else recateg = ".*";
+
+		if(cl.hasOption("j")) ifjap = true;
+		else ifjap = false;
+
+		if(cl.hasOption("v")) ifverb = true;
+		else ifverb = false;
+
+	}
+}
+
+class Env {
+	int numofcpus;
+}
+
+abstract class AbstParse {
+
+	List<String> stopwords;
+
+	boolean ifPageStart(String line){
+		return line.indexOf("<page")>=0;
 	}
 
-	void bowCreater(String text){
+	boolean ifPageEnd(String line){
+		return line.indexOf("</page>")>=0;
+	}
 
-		Map<String,Integer> mapBow = new HashMap<String,Integer>();
+	abstract boolean ifjap();
+	abstract void holdStopwords(String str);
 
-		Pattern pattern = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$");
-		List<String> notWord = new ArrayList<String>();
+}
 
-		int wordcount = 0;
+// Singleton
+class EngParse extends AbstParse {
+
+	Map<String,String> mapDict = new HashMap<String,String>();
+
+	private static final EngParse instance = new EngParse();
+
+	private EngParse(){
+		holdStopwords(
+			"a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+		);
+	}
+
+	@Override
+	void holdStopwords(String str){
+		stopwords= Arrays.asList( str.split(",") );
+	}
+
+	@Override
+	boolean ifjap(){ return false; }
+
+	public static EngParse getInstance(){
+		return instance;
+	}
+
+	void getEngMapDict(String file) throws IOException {
+
+		String line;
+		try( BufferedReader br = new BufferedReader(new FileReader(file)) ){
+			while((line=br.readLine())!=null){
+				if(line.indexOf(";;;")>=0) continue;
+				String cols[] = line.split("\\t\\t");
+				mapDict.put(cols[0],cols[1]);
+			}
+		} catch (IOException e){
+			throw e;
+		}
+
+	}
+}
+
+// Singleton
+class JapParse extends AbstParse {
+
+	private static final JapParse instance = new JapParse();
+
+	private JapParse(){}
+
+	public static JapParse getInstance(){
+		return instance;
+	}
+
+	@Override
+	void holdStopwords(String str){
+		stopwords= Arrays.asList( str.split(",") );
+	}
+
+	@Override
+	boolean ifjap(){ return true; }
+}
+
+class RunParse implements Runnable {
+
+	String page;
+	ArgStore argstore;
+	BufferedWriter bw;
+	AbstParse parse;
+
+	private final static Object lock = new Object();
+
+	public RunParse(String page, ArgStore argstore, BufferedWriter bw, AbstParse parse){
+		this.page= page;
+		this.argstore= argstore;
+		this.bw= bw;
+		this.parse= parse;
+	}
+
+	void bowCreator(String text){
+
+		Map<String,Integer> mapbow = new HashMap<String,Integer>();
+
+		Pattern wpat= Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$");
+		List<String> notwords= new ArrayList<String>();
+
+		int wordcnt= 0;
 		for(String word: text.split(" ")){
 
-			if(stopwords.contains(word)) continue;
+			if(parse.stopwords.contains(word)) continue;
+			if(notwords.contains(word))  continue;
 
-			if(notWord.contains(word)) continue;
-
-			Matcher matcher = pattern.matcher(word);
-			if(!matcher.find()){
-				notWord.add(word);
+			Matcher wmat= wpat.matcher(word);
+			if(!wmat.find()){
+				notwords.add(word);
 				continue;
 			}
 
 			if(word.length()==1) continue;
 
 			String bowWord;
-			if(mapDict.containsKey(word)){
-				bowWord=mapDict.get(word);
-			}else{
+			if(parse.ifjap()){
 				bowWord = word;
-			}
-
-			if(mapBow.containsKey(bowWord)){
-				mapBow.put(bowWord,mapBow.get(bowWord)+1);
 			}else{
-				mapBow.put(bowWord,1);
+				EngParse engparse = (EngParse)parse;
+				if(engparse.mapDict.containsKey(word)){
+					bowWord= engparse.mapDict.get(word);
+				}else{
+					bowWord = word;
+				}
 			}
-			wordcount+=1;
 
-			if(wordcount>maxl) return;
+			if(mapbow.containsKey(bowWord)){
+				mapbow.put(bowWord,mapbow.get(bowWord)+1);
+			}else{
+				mapbow.put(bowWord,1);
+			}
+			wordcnt+=1;
+
+			if(wordcnt>argstore.maxl) return;
 		}
-		if(wordcount<minl) return;
+		if(wordcnt<argstore.minl) return;
 
-		int count=0;
+		int cnt=0;
 		StringBuffer bowBuf = new StringBuffer("");
-		for(Map.Entry<String,Integer> entry: mapBow.entrySet()){
-			if(entry.getValue()<minc) continue;
-			if(count>0) bowBuf.append(" ");
+		for(Map.Entry<String,Integer> entry: mapbow.entrySet()){
+			if(entry.getValue()<argstore.minc) continue;
+			if(cnt>0) bowBuf.append(" ");
 			bowBuf.append(String.format("%s %d",entry.getKey(),entry.getValue()));
-			count+=1;
+			cnt+=1;
 		}
 		bowBuf.append("\n");
 
@@ -96,28 +225,25 @@ class BOW implements Runnable {
 	}
 
 	public void run(){
-		Pattern cpattern = Pattern.compile("\\[\\[:*Category:([^\\[\\]\\|]+)\\|*[^\\[\\]]*\\]\\]");
-		Matcher cmatcher = cpattern.matcher(pageStr);
+		Pattern cpat= Pattern.compile("\\[\\[:*Category:([^\\[\\]\\|]+)\\|*[^\\[\\]]*\\]\\]");
+		Matcher cmat= cpat.matcher(page);
 
-		Boolean categoryMatch = false;
-		while(cmatcher.find()){
-			String categoryStr = cmatcher.group(1);
-			Pattern patternCategory = Pattern.compile(categoryRe);
-			Matcher matcherCategory = patternCategory.matcher(categoryStr);
-			if(matcherCategory.find()){
-				categoryMatch = true;
-				break;
-			}
+		boolean cmatflag= false;
+		while(cmat.find()){
+			String category= cmat.group(1);
+			Pattern rcpat= Pattern.compile(argstore.recateg);
+			Matcher rcmat= rcpat.matcher(category);
+			if(rcmat.find()){ cmatflag= true; break; }
 		}
-		if(!categoryMatch) return;
+		if(!cmatflag) return;
 
-		Pattern pattern = Pattern.compile("<text[^<>]*>([^<>]+)</text>");
-		Matcher matcher = pattern.matcher(pageStr);
+		Pattern tpat= Pattern.compile("<text[^<>]*>([^<>]+)</text>");
+		Matcher tmat= tpat.matcher(page);
 
-		String text = "";
-		if(matcher.find()){
-			text = matcher.group(1);
-			bowCreater(text);
+		String text= "";
+		if(tmat.find()){
+			text= tmat.group(1);
+			bowCreator(text);
 		}
 	}
 }
@@ -137,88 +263,67 @@ public class ParseWikipediaXML {
 		options.addOption("x","max-page-words",true,"Maxmum number of words that a page should have");
 		options.addOption("c","min-word-count",true,"Minimum number that a word should have");
 		options.addOption("g","category-regex",true,"Category in regular expression");
+		options.addOption("j","japanese",true,"Flag for Japanese");
+		options.addOption("v","verbose",true,"Verbose");
 
 		HelpFormatter help = new HelpFormatter();
 
-		String ifwiki,ifdict,ofcont,oftitle,categoryRe;
-		ifwiki=ifdict=ofcont=oftitle=categoryRe="";
-
-		Integer minl,maxl,minc;
-		minl=maxl=minc=0;
-
+		ArgStore argstore= null;
 		try{
-
 			CommandLine cl = parser.parse(options, args);
-
-			if(cl.hasOption("i")) ifwiki = cl.getOptionValue("i");
-			else throw new ParseException("i is not specified.");
-
-			if(cl.hasOption("d")) ifdict = cl.getOptionValue("d");
-			else throw new ParseException("d is not specified.");
-
-			if(cl.hasOption("s")) ofcont = cl.getOptionValue("s");
-			else throw new ParseException("s is not specified.");
-
-			if(cl.hasOption("t")) oftitle= cl.getOptionValue("t");
-			//else throw new ParseException("t is not specified.");
-
-			if(cl.hasOption("m")) minl = Integer.parseInt(cl.getOptionValue("m"));
-			if(cl.hasOption("x")) maxl = Integer.parseInt(cl.getOptionValue("x"));
-			if(cl.hasOption("c")) minc = Integer.parseInt(cl.getOptionValue("c"));
-
-			if(cl.hasOption("g")) categoryRe = cl.getOptionValue("g");
-			else categoryRe = ".*";
-
+			argstore= new ArgStore(cl);
 		} catch (ParseException e){
 			help.printHelp("ParseWikipediaXML",options);
 			System.exit(1);
 		}
 
+
+		AbstParse parse;
+		if(argstore.ifjap){
+			parse = JapParse.getInstance();
+		}else{
+			parse = EngParse.getInstance();
+		}
+
+		BufferedWriter bw = null;
 		try{
-			BOW.bw = new BufferedWriter(new FileWriter(ofcont));
+			bw = new BufferedWriter(new FileWriter(argstore.ofcont));
 		} catch (IOException e){
 			System.exit(13);
 		}
 
-		int cpu = Runtime.getRuntime().availableProcessors();
-		System.out.printf("# of CPU is %d.\n",cpu);
-		ExecutorService ex = Executors.newFixedThreadPool(cpu);
+		Env env = new Env();
+		env.numofcpus = Runtime.getRuntime().availableProcessors();
+		if(argstore.ifverb) System.out.printf("# of CPU is %d.\n",env.numofcpus);
 
-		String line;
-		try( BufferedReader br = new BufferedReader(new FileReader(ifdict)) ){
-			while((line=br.readLine())!=null){
-				if(line.indexOf(";;;")>=0) continue;
-				String cols[] = line.split("\\t\\t");
-				BOW.mapDict.put(cols[0],cols[1]);
+		ExecutorService ex = Executors.newFixedThreadPool(env.numofcpus);
+
+		if(!argstore.ifjap){
+			EngParse engparse = (EngParse)parse;
+			try{
+				engparse.getEngMapDict(argstore.ifdict);
+			} catch (IOException e){
+				System.exit(11);
 			}
-		} catch (IOException e){
-			System.exit(11);
 		}
 
-		String stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your";
-		BOW.stopwords = Arrays.asList(stopwords.split(","));
+		try( BufferedReader br = new BufferedReader(new FileReader(argstore.ifwiki)) ){
 
-		try( BufferedReader br = new BufferedReader(new FileReader(ifwiki)) ){
+			StringBuffer buf = null;
+			String line;
+			boolean sflag=false, eflag=false;
 
-			StringBuffer pageBuf = new StringBuffer("");
-			Boolean startFlag, endFlag;
-
-			startFlag=endFlag=false;
 			while((line=br.readLine())!=null){
 
-				if(line.indexOf("<page")>=0){
-					startFlag = true;
-				}
-				if(line.indexOf("</page>")>=0){
-					endFlag= true;
-				}
+				if(parse.ifPageStart(line)) sflag = true;
+				if(parse.ifPageEnd(line))   eflag = true;
 
-				if(startFlag){
-					pageBuf.append(line);
-					if(endFlag){
-						ex.execute(new BOW(pageBuf.toString(),categoryRe,minl,maxl,minc));
-						startFlag=endFlag=false;
-						pageBuf.delete(0, pageBuf.length());
+				if(sflag){
+					buf.append(line);
+					if(eflag){
+						ex.execute(new RunParse(buf.toString(),argstore,bw,parse));
+						sflag=eflag=false;
+						buf.delete(0, buf.length());
 					}
 				}
 			}
