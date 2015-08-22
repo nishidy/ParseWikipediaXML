38617ac7fd85c830b40f0a8f217381180b56cc2f
nishidy <nishidyatutokyo@gmail.com>
2015-08-22 22:05:09 +0900

    Update base classes and sub classes again.

--- java/ParseWikipediaXML.java
+++ java/ParseWikipediaXML.java
@@ -18,18 +18,18 @@ import org.atilika.kuromoji.Tokenizer;
 
 class ArgStore {
 
-	String ifwiki;
-	String ifdict;
-	String ofcont;
-	String oftitle;
-	String recateg;
-	int minl;
-	int maxl;
-	int minc;
-	boolean ifjap;
-	boolean ifverb;
+	static String ifwiki;
+	static String ifdict;
+	static String ofcont;
+	static String oftitle;
+	static String recateg;
+	static int minl;
+	static int maxl;
+	static int minc;
+	static boolean isJap;
+	static boolean isVerb;
 
-	ArgStore(CommandLine cl) throws ParseException {
+	static void init(CommandLine cl) throws ParseException {
 
 		if(cl.hasOption("i")) ifwiki = cl.getOptionValue("i");
 		else throw new ParseException("i is not specified.");
@@ -55,22 +55,21 @@ class ArgStore {
 		if(cl.hasOption("g")) recateg = cl.getOptionValue("g");
 		else recateg = ".*";
 
-		if(cl.hasOption("j")) ifjap = true;
-		else ifjap = false;
+		if(cl.hasOption("j")) isJap= true;
+		else isJap= false;
 
-		if(cl.hasOption("v")) ifverb = true;
-		else ifverb = false;
+		if(cl.hasOption("v")) isVerb = true;
+		else isVerb = false;
 
 	}
 }
 
-class Env {
-	int numofcpus;
-}
-
 abstract class AbstParse {
 
-	List<String> stopwords;
+	abstract void createBaseWords(String file) throws IOException;
+	abstract String convertToBaseWord(String line);
+	abstract boolean isJap();
+	abstract boolean isCommonWord(String word);
 
 	boolean ifPageStart(String line){
 		return line.indexOf("<page")>=0;
@@ -80,37 +79,32 @@ abstract class AbstParse {
 		return line.indexOf("</page>")>=0;
 	}
 
-	abstract boolean ifjap();
-	abstract void holdStopwords(String str);
-
 }
 
 // Singleton
 class EngParse extends AbstParse {
 
-	Map<String,String> mapDict = new HashMap<String,String>();
+	private EngParse(){
+		stopwords= Arrays.asList( "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(",") );
+	}
 
 	private static final EngParse instance = new EngParse();
+	public static EngParse getInstance(){ return instance; }
 
-	private EngParse(){
-		holdStopwords(
-			"a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
-		);
+	List<String> stopwords = null;
+	Map<String,String> mapDict = new HashMap<String,String>();
+
+	@Override
+	boolean isJap(){ return false; }
+
+	@Override
+	boolean isCommonWord(String word){
+		if(stopwords.contains(word)) return true;
+		return false;
 	}
 
 	@Override
-	void holdStopwords(String str){
-		stopwords= Arrays.asList( str.split(",") );
-	}
-
-	@Override
-	boolean ifjap(){ return false; }
-
-	public static EngParse getInstance(){
-		return instance;
-	}
-
-	void getEngMapDict(String file) throws IOException {
+	void createBaseWords(String file) throws IOException {
 
 		String line;
 		try( BufferedReader br = new BufferedReader(new FileReader(file)) ){
@@ -124,76 +118,94 @@ class EngParse extends AbstParse {
 		}
 
 	}
+
+	@Override
+	String convertToBaseWord(String word) {
+		if(mapDict.containsKey(word)){
+			return mapDict.get(word);
+		}else{
+			return word;
+		}
+	}
+
 }
 
 // Singleton
 class JapParse extends AbstParse {
 
-	private static final JapParse instance = new JapParse();
-
 	private JapParse(){}
 
-	public static JapParse getInstance(){
-		return instance;
+	private static final JapParse instance = new JapParse();
+	public static JapParse getInstance(){ return instance; }
+
+	@Override
+	boolean isJap(){ return true; }
+
+	@Override
+	String convertToBaseWord(String word) {
+		return word;
 	}
 
 	@Override
-	void holdStopwords(String str){
-		stopwords= Arrays.asList( str.split(",") );
-	}
+	boolean isCommonWord(String word){ return false; }
 
 	@Override
-	boolean ifjap(){ return true; }
+	void createBaseWords(String file) { }
+
 }
+
+//class ChiParse extends AbstParse {
 
 class RunParse implements Runnable {
 
 	String page;
-	ArgStore argstore;
 	BufferedWriter bw;
 	AbstParse parse;
 
+	List<String> notwords= new ArrayList<String>();
+
 	private final static Object lock = new Object();
 
-	public RunParse(String page, ArgStore argstore, BufferedWriter bw, AbstParse parse){
+	public RunParse(String page, BufferedWriter bw, AbstParse parse){
 		this.page= page;
-		this.argstore= argstore;
 		this.bw= bw;
 		this.parse= parse;
+	}
+
+	boolean isNotWord(String word){
+		if(word.length()==1) return true;
+		return false;
+	}
+
+	boolean isInNotWordList(String word){
+		if(notwords.contains(word)) return true;
+		return false;
+	}
+
+	boolean isWordAndUpdateList(String word){
+		Pattern wpat= Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$");
+		Matcher wmat= wpat.matcher(word);
+		if(wmat.find()){
+			return true;
+		}else{
+			notwords.add(word);
+			return false;
+		}
 	}
 
 	void bowCreator(String text){
 
 		Map<String,Integer> mapbow = new HashMap<String,Integer>();
 
-		Pattern wpat= Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$");
-		List<String> notwords= new ArrayList<String>();
-
 		int wordcnt= 0;
 		for(String word: text.split(" ")){
 
-			if(parse.stopwords.contains(word)) continue;
-			if(notwords.contains(word))  continue;
+			if(isNotWord(word)) continue;
+			if(isInNotWordList(word)) continue;
+			if(!isWordAndUpdateList(word)) continue;
+			if(parse.isCommonWord(word)) continue;
 
-			Matcher wmat= wpat.matcher(word);
-			if(!wmat.find()){
-				notwords.add(word);
-				continue;
-			}
-
-			if(word.length()==1) continue;
-
-			String bowWord;
-			if(parse.ifjap()){
-				bowWord = word;
-			}else{
-				EngParse engparse = (EngParse)parse;
-				if(engparse.mapDict.containsKey(word)){
-					bowWord= engparse.mapDict.get(word);
-				}else{
-					bowWord = word;
-				}
-			}
+			String bowWord= parse.convertToBaseWord(word);
 
 			if(mapbow.containsKey(bowWord)){
 				mapbow.put(bowWord,mapbow.get(bowWord)+1);
@@ -202,9 +214,13 @@ class RunParse implements Runnable {
 			}
 			wordcnt+=1;
 
-			if(wordcnt>argstore.maxl) return;
+			if(ArgStore.isVerb){
+				System.out.printf("%s -> %s.\n",word,bowWord);
+			}
+
+			if(wordcnt>ArgStore.maxl) return;
 		}
-		if(wordcnt<argstore.minl) return;
+		if(wordcnt<ArgStore.minl) return;
 
 		//List<Map.Entry<String,Integer>> listmapbow = new ArrayList<Map.Entry<String,Integer>>(mapbow.entrySet())
 		List<Map.Entry<String,Integer>> entries= new ArrayList<>(mapbow.entrySet());
@@ -218,7 +234,7 @@ class RunParse implements Runnable {
 		int cnt=0;
 		StringBuffer bowBuf = new StringBuffer("");
 		for(Map.Entry<String,Integer> entry: entries){
-			if(entry.getValue()<argstore.minc) continue;
+			if(entry.getValue()<ArgStore.minc) continue;
 			if(cnt>0) bowBuf.append(" ");
 			bowBuf.append(String.format("%s %d",entry.getKey(),entry.getValue()));
 			cnt+=1;
@@ -243,7 +259,7 @@ class RunParse implements Runnable {
 		boolean cmatflag= false;
 		while(cmat.find()){
 			String category= cmat.group(1);
-			Pattern rcpat= Pattern.compile(argstore.recateg);
+			Pattern rcpat= Pattern.compile(ArgStore.recateg);
 			Matcher rcmat= rcpat.matcher(category);
 			if(rcmat.find()){ cmatflag= true; break; }
 		}
@@ -280,10 +296,9 @@ public class ParseWikipediaXML {
 
 		HelpFormatter help = new HelpFormatter();
 
-		ArgStore argstore= null;
 		try{
 			CommandLine cl = parser.parse(options, args);
-			argstore= new ArgStore(cl);
+			ArgStore.init(cl);
 
 		} catch (ParseException e){
 			help.printHelp("ParseWikipediaXML",options);
@@ -292,7 +307,7 @@ public class ParseWikipediaXML {
 
 
 		AbstParse parse;
-		if(argstore.ifjap){
+		if(ArgStore.isJap){
 			parse = JapParse.getInstance();
 		}else{
 			parse = EngParse.getInstance();
@@ -300,8 +315,8 @@ public class ParseWikipediaXML {
 
 		BufferedWriter bw = null;
 		try{
-			if(argstore.ofcont!=null){
-				bw = new BufferedWriter(new FileWriter(argstore.ofcont));
+			if(ArgStore.ofcont!=null){
+				bw = new BufferedWriter(new FileWriter(ArgStore.ofcont));
 			}else{
 				bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			}
@@ -309,22 +324,18 @@ public class ParseWikipediaXML {
 			System.exit(13);
 		}
 
-		Env env = new Env();
-		env.numofcpus = Runtime.getRuntime().availableProcessors();
-		if(argstore.ifverb) System.out.printf("# of CPU is %d.\n",env.numofcpus);
+		int numofcpus = Runtime.getRuntime().availableProcessors();
+		if(ArgStore.isVerb) System.out.printf("# of CPU is %d.\n",numofcpus);
 
-		ExecutorService ex = Executors.newFixedThreadPool(env.numofcpus);
+		ExecutorService ex = Executors.newFixedThreadPool(numofcpus);
 
-		if(!argstore.ifjap){
-			EngParse engparse = (EngParse)parse;
-			try{
-				engparse.getEngMapDict(argstore.ifdict);
-			} catch (IOException e){
-				System.exit(11);
-			}
+		try{
+			parse.createBaseWords(ArgStore.ifdict);
+		} catch (IOException e){
+			System.exit(11);
 		}
 
-		try( BufferedReader br = new BufferedReader(new FileReader(argstore.ifwiki)) ){
+		try( BufferedReader br = new BufferedReader(new FileReader(ArgStore.ifwiki)) ){
 
 			StringBuffer buf = new StringBuffer("");
 			String line;
@@ -338,7 +349,7 @@ public class ParseWikipediaXML {
 				if(sflag){
 					buf.append(line);
 					if(eflag){
-						ex.execute(new RunParse(buf.toString(),argstore,bw,parse));
+						ex.execute(new RunParse(buf.toString(),bw,parse));
 						sflag=eflag=false;
 						buf.delete(0, buf.length());
 					}
