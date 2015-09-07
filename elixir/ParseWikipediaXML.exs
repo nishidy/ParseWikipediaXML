3d597d8c4d72128b5c82490eb93a2069c8e6e5d9
nishidy <nishidyatutokyo@gmail.com>
2015-09-08 00:10:47 +0900

    Supported N-gram in elixir.

--- elixir/ParseWikipediaXML.exs
+++ elixir/ParseWikipediaXML.exs
@@ -34,6 +34,7 @@ defmodule Main do
 
   def parse(text) do
     minc = 2
+    ngram_len=2
 
     stopwords = String.split("a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your",",")
 
@@ -51,6 +52,7 @@ defmodule Main do
         end
       end
     )
+    |> listup_ngram([],ngram_len,[])
     |> Enum.reduce( HashDict.new,
       fn x, bofw ->
         if Dict.has_key?(bofw,x),
@@ -64,6 +66,23 @@ defmodule Main do
     |> Enum.join( " ")
     |> output
 
+  end
+
+  def listup_ngram([h|t],ngram,len,ngram_list) when length(ngram) < len do
+    listup_ngram(t,[h|ngram],len,ngram_list)
+  end
+
+  def listup_ngram([h|t],ngram,len,ngram_list) when length(ngram) == len do
+    listup_ngram(
+      t,
+      [h|List.delete_at(ngram,len-1)],
+      len,
+      [Enum.join(Enum.reverse(ngram),":")|ngram_list]
+    )
+  end
+
+  def listup_ngram([],_,_,ngram_list) do
+    ngram_list
   end
 
   def output(text) do
