e742c463da7876f9f633907262110568cda3f2b3
nishidy <nishidyatutokyo@gmail.com>
2015-09-11 02:41:45 +0900

    Applied kagome package to tokenize Japanese.

--- go/ParseWikipediaXML.go
+++ go/ParseWikipediaXML.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	//"io/ioutil"
 	//"compress/bzip2"
@@ -14,9 +13,32 @@ import (
 	"runtime"
 	"strings"
 	//"sync"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/ikawaha/kagome"
 )
 
 var stopwords string = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+
+// The name of this function should be changed
+func MakeMapJP(text string, dict map[string]string, stopWords []string) (int, map[string]int) {
+
+	tkn := kagome.NewTokenizer()
+	morphs := tkn.Tokenize(text)
+
+	var wc = 0
+	var mapWordCnt = make(map[string]int)
+
+	for _, m := range morphs {
+		f := m.Features()
+		if strings.Contains(f[0], "名詞") {
+			mapWordCnt[m.Surface] = 1
+		}
+
+	}
+
+	return wc, mapWordCnt
+}
 
 // The name of this function should be changed
 func MakeMap(text string, dict map[string]string, stopWords []string) (int, map[string]int) {
@@ -203,6 +225,7 @@ func main() {
 	var minc = flag.Int("c", 2, "Minimum number that a word should have")
 	var cat = flag.String("g", ".*", "Category(regular expression)")
 	var json = flag.Bool("j", false, "Generate bug-of-words in JSON format")
+	var jp = flag.Bool("p", false, "If this is for Japanese text")
 
 	flag.Parse()
 
@@ -272,8 +295,15 @@ func main() {
 
 				var text_map = make(map[string]int)
 				var wc = 0
-				if wc, text_map = MakeMap(text, dict, stopWords); wc < *minl || wc > *maxl {
-					continue
+
+				if *jp {
+					if wc, text_map = MakeMap(text, dict, stopWords); wc < *minl || wc > *maxl {
+						continue
+					}
+				} else {
+					if wc, text_map = MakeMapJP(text, dict, stopWords); wc < *minl || wc > *maxl {
+						continue
+					}
 				}
 
 				/* Only one thread can write the result into file at once */
