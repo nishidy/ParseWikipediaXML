068d6591eeeeedb01fb40b85eec192b281f9e6cb
nishidy <nishidyatutokyo@gmail.com>
2015-10-25 12:12:18 +0900

    Created a program to check diff between two files without checking the order that each line appear in the file. It is also integrated in test shell.

---
+++ share/diffOfLines.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string inputFile1,inputFile2;
+	if(argc==3){
+		inputFile1 = argv[1];
+		inputFile2 = argv[2];
+	}else{
+		cout << argv[0] << " inputFile1 inputFile2"<< endl;
+		return -1;
+	}
+
+	ifstream ifs1(inputFile1);
+	if(ifs1.fail()){
+		cerr << "Failed to open " << inputFile1 << "." << endl;
+		return -1;
+	}
+
+	ifstream ifs2(inputFile2);
+	if(ifs2.fail()){
+		cerr << "Failed to open " << inputFile2 << "." << endl;
+		return -1;
+	}
+
+	vector<int> vec_int_file1;
+	vector<int> vec_int_file2;
+	string line;
+
+	while(getline(ifs1, line)){
+		if(line.size()>0)
+			vec_int_file1.push_back(hash<string>()(line));
+	}
+
+	while(getline(ifs2, line)){
+		if(line.size()>0)
+			vec_int_file2.push_back(hash<string>()(line));
+	}
+
+	sort(vec_int_file1.begin(),vec_int_file1.end());
+	sort(vec_int_file2.begin(),vec_int_file2.end());
+
+	if( vec_int_file1 != vec_int_file2 ){
+		return 1;
+	}
+
+	return 0;
+}
--- share/testParseWikipediaXML.sh
+++ share/testParseWikipediaXML.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -eu
 
-[[ ${#@} -eq 3 ]] || exit 1
+[[ ${#@} -eq 4 ]] || { echo "$0 \"prog\" numTests numWorkers numDocs"; exit 1; }
 
 prog=$1
-numTest=$2
-numDoc=$3
+numTests=$2
+numWorkers=$3
 
-[[ -d "test" ]] && { echo "Must have clean 'test' directory."; exit 2; }
+numDocs=$4
+maxNumTermsInDocs=1000
+maxNumCharOfTerm=20
+maxFreqOfTerm=100
+
+[[ -d "test" ]] && {
+	echo "Must have clean 'test' directory."
+	echo -n "If you want to destroy it, enter 'Go!'. : "
+	read input
+	if [[ $input == "Go!" ]]; then
+		rm -rf test
+		[[ -d "test" ]] && { echo "Could not destroy it..."; exit 3; }
+	else
+		echo "Bye."
+		exit 2
+	fi
+}
 
 mkdir -p test
 
@@ -14,7 +30,7 @@ bofwfile="test/bofw"
 docfile="test/doc"
 outfile="test/out"
 
-for n in $(seq 1 $numTest);do
+for n in $(seq 1 $numTests);do
 	echo "[Test #${n}]"
 
 	nthbofwfile="$bofwfile$n.txt"
@@ -24,16 +40,16 @@ for n in $(seq 1 $numTest);do
 	#./makeTestBagofwords numDocs maxNumTermsInDocs maxNumCharOfTerm maxFreq
 
 	echo "Make bag-of-words file $nthbofwfile."
-	./makeTestBagofwords $numDoc 1000 20 100 > $nthbofwfile
+	./makeTestBagofwords $numDocs $maxNumTermsInDocs $maxNumCharOfTerm $maxFreqOfTerm > $nthbofwfile
 
 	echo "Make documents file $nthdocfile from $nthbofwfile."
 	./makeTestDocuments $nthbofwfile > $nthdocfile
 
 	echo "Run '$prog' to make bag-of-wrods file from $nthdocfile."
-	$prog -i "$nthdocfile" -s "$nthoutfile" -c 1
+	$prog -i "$nthdocfile" -s "$nthoutfile" -c 1 -w $numWorkers
 
 	echo "Check diff betwen $nthbofwfile and $nthoutfile."
-	if diff $nthbofwfile $nthoutfile > /dev/null; then
+	if ./diffOfLines $nthbofwfile $nthoutfile; then
 		echo "OK. '$prog' is stable."
 	else
 		echo "NG. '$prog' is not stable. Please check the program. Aborted."
