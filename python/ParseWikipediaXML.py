fbcf793b55d957ca9a3480154f1315fd445b6603
nishidy <nishidyatutokyo@gmail.com>
2015-11-06 23:59:20 +0900

    Decorator shows how long it takes.

--- python/ParseWikipediaXML.py
+++ python/ParseWikipediaXML.py
@@ -168,6 +168,17 @@ class JapParser(AbstParser):
 
 		return dictBofw
 
+def check(func):
+	import functools
+	@functools.wraps(func)
+	def wrapper(*args,**kwargs):
+		import time
+		start = time.time() * 1000
+		print("Begin %s."%(func.__name__))
+		func(*args,**kwargs)
+		print("Finished %s in %d msec."%(func.__name__,time.time()*1000-start))
+	return wrapper
+
 class EngParser(AbstParser):
 
 	def __init__(self,args):
@@ -176,11 +187,10 @@ class EngParser(AbstParser):
 		stopwords="a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
 		self.stopwords=stopwords.split(",")
 
+	@check
 	def readDictionary(self):
 		self.dictMap = {}
 		if self.args.ifdict == "": return
-
-		print("Begin reading dictionary...", end=" ")
 
 		# try message pack format first
 		try:
@@ -205,8 +215,6 @@ class EngParser(AbstParser):
 		except IOError as e:
 			print(e,file=sys.stderr)
 			sys.exit(1)
-
-		print("Finish.")
 
 	def parseText(self,text):
 
