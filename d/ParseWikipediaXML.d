8d484405d732c45b0fdc819e0d9c2361a9027079
nishidy <nishidyatutokyo@gmail.com>
2015-11-21 22:52:12 +0900

    Exclude stopwords.

--- d/ParseWikipediaXML.d
+++ d/ParseWikipediaXML.d
@@ -5,6 +5,8 @@ struct Bofw {
 	uint freq;
 };
 
+auto stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(",");
+
 void main(string argv[]) {
 
 	if(argv.length!=3){
@@ -47,7 +49,12 @@ Bofw[] parsePage(char[] page){
 	Bofw[] bofw;
 	ulong[string] bofwIndex;
 
-	foreach(string word; page.split().map!(to!string).filter!matchWord()){
+	foreach(string word;
+			page.split()
+			.map!(to!string)
+			.filter!matchWord()
+			.filter!(a=>!stopwords.canFind(a))
+	){
 		if(word in bofwIndex){
 			++bofw[bofwIndex[word]].freq;
 		}else{
