62676f75460343a7ff72910b8bab42dff13059ff
nishidy <nishidyatutokyo@gmail.com>
2015-11-21 20:40:29 +0900

    Save output as string.

--- d/ParseWikipediaXML.d
+++ d/ParseWikipediaXML.d
@@ -1,33 +1,49 @@
-import std.stdio, std.string, std.conv, std.regex, std.algorithm;
+import std.stdio, std.string, std.conv, std.regex, std.algorithm, std.c.stdlib;
 
-void main() {
+void main(string argv[]) {
 
-	auto file = File("../share/enwiki-test-5000");
+	if(argv.length!=3){
+		writef("%s\n inFile outFile",argv[0]);
+		exit(-1);
+	}
+
+	auto infile = File(argv[1]);
+	auto outfile = File(argv[2],"w");
 
 	bool beginpage = false, endpage = false;
 	char[] page;
 
-	foreach(line; file.byLine() ) {
+	foreach(char[] line; infile.byLine() ) {
 		if(line.indexOf("<page>")>-1) beginpage = true;
 		if(line.indexOf("</page>")>-1) endpage = true;
 
 		if(beginpage) page ~= line;
 		if(endpage){
-			auto hashDict = parsePage(page);
-			writeln(hashDict);
+			auto hashBofw= parsePage(page);
+			outfile.writef("%s\n",hashToString(hashBofw));
 			beginpage=endpage=false;
 		}
 	}
 
 }
 
+string hashToString(int[string] hashBofw){
+	char[] sliceBofw;
+	foreach(k,v;hashBofw) {
+		sliceBofw ~= k ~ " " ~ to!string(v) ~ " ";
+	}
+	--sliceBofw.length;
+
+	return to!string(sliceBofw);
+}
+
 int[string] parsePage(char[] page){
 
 	int[string] hashDict;
 
-	foreach(word; page.split().map!(to!string).filter!matchWord()){
+	foreach(string word; page.split().map!(to!string).filter!matchWord()){
 		if(word in hashDict){
-			hashDict[word] += 1;
+			++hashDict[word];
 		}else{
 			hashDict[word] = 1;
 		}
@@ -40,3 +56,4 @@ int[string] parsePage(char[] page){
 bool matchWord(string word){
 	return match(word, r"^[a-z][a-z0-9'-]*[a-z0-9]$").to!bool;
 }
+
