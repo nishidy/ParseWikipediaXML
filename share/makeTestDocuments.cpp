4d39749879d343df65e282353187d6b41e869617
nishidy <nishidyatutokyo@gmail.com>
2015-11-23 08:05:30 +0900

    Append one of stopwords.

--- share/makeTestDocuments.cpp
+++ share/makeTestDocuments.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 #define ASCII_DIFF_UPPER_AND_LOWER 32
 
-string makeDocFromGroupOfBofw(string line){
+string stopwords= "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your";
+
+string makeDocFromGroupOfBofw(string line, vector<string> *vecStopwords){
 
 	stringstream ss(line);
 	string term;
@@ -32,6 +34,8 @@ string makeDocFromGroupOfBofw(string line){
 			vecTerms.push_back(tmpCurrentTerm);
 		}
 	}
+	// Append one of stopwords
+	vecTerms.push_back(vecStopwords->at(line.length()%vecStopwords->size()));
 
 	for(int i=0;i<log(vecTerms.size())+1;i++){
 		random_shuffle(vecTerms.begin(),vecTerms.end());
@@ -62,12 +66,19 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 
+	stringstream ss(stopwords);
+	string word;
+	vector<string> vecStopwords;
+	while(getline(ss,word,',')){
+		vecStopwords.push_back(word);
+	}
+
 	string line;
 	int cnt=0;
 	while(getline(ifs, line)){
 		cout << "<page>" << endl << "<title>title" << cnt << "</title><text>";
-		cout << makeDocFromGroupOfBofw(line) << endl;
-		cout << "</text>"<< endl << "<category>category" << cnt << "</category>" << "</page>" << endl;
+		cout << makeDocFromGroupOfBofw(line,&vecStopwords) << endl;
+		cout << "</text>"<< endl << "</page>" << endl;
 		cnt++;
 	}
 
