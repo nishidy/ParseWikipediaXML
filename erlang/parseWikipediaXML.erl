7c5a1e89577d31162fb0f38d793932b33e6f5939
nishidy <nishidyatutokyo@gmail.com>
2015-12-19 21:39:59 +0900

    Fixed that does not match to a character. ' ' should be $\ and '\n' should be $\n. Additionally, optimized chomp function with reverse.

--- erlang/parseWikipediaXML.erl
+++ erlang/parseWikipediaXML.erl
@@ -1,7 +1,7 @@
 -module(parseWikipediaXML).
 -export([main/0]).
 -import(string,[tokens/2,to_lower/1,to_integer/1,join/2]).
--import(lists,[sublist/3,concat/1,map/2,filter/2,nth/2,any/2,reverse/1,keyfind/3,keydelete/3]).
+-import(lists,[sublist/3,concat/1,map/2,filter/2,nth/2,any/2,reverse/1,keyfind/3,keydelete/3,reverse/1]).
 -import(proplists,[lookup/2]).
 
 %$ erl -noshell -s parseWikipediaXML main -i ../share/enwiki-test-5000 -o o1 -s init stop
@@ -12,6 +12,7 @@ main() ->
     %io:format("~p.~n",[Args]),
 
     Dict = read_dictionary(Args),
+    io:format("~p~n",[Dict]),
 
     case keyfind(fhi,1,Args) of
         false -> error(badarg);
@@ -22,27 +23,33 @@ main() ->
 read_dictionary(Args) ->
     Usec = 1000000.0,
     {_,S,US} = now(),
-    case keyfind(fhd,1,Args) of
+    Props = case keyfind(fhd,1,Args) of
         false -> error(badarg);
-        {fhd,Fhd} ->
-            read_baseforms(Fhd, [], 0)
+        {fhd,Fhd} -> read_baseforms(Fhd, [], 0)
     end,
     {_,F,UF} = now(),
-    io:format("~n > Read dictionary in ~.3f sec.~n",[((F*Usec+UF)-(S*Usec+US))/Usec]).
+    io:format("~n > Read dictionary in ~.3f sec.~n",[((F*Usec+UF)-(S*Usec+US))/Usec]),
+    Props.
 
 read_baseforms(Fhd, Props, Cnt) ->
-    io:format(" > Read dictionary [ # word ~.10B ]\r", [Cnt]),
+    %io:format(" > Read dictionary [ # word ~.10B ]\r", [Cnt]),
     L = line_read(Fhd),
     case L of
-        eof -> unit;
-        _->
-            Newprops = case tokens(L," \t") of
-                [P|[B|_]] -> [{P,B},Props];
-                _ -> Props
-            end,
-            read_baseforms(Fhd, Newprops, Cnt+1)
+        eof -> Props;
+        _-> get_baseform(Fhd, Props, Cnt, L)
+    end.
+
+get_baseform(Fhd, Props, Cnt, L) ->
+    Newprops = case tokens(L,"\t") of
+        [";;;"|_] -> Props;
+        [P|[B|_]] ->
+            case any(fun(X)-> X==$\ end, B) of
+                true -> Props;
+                false -> [{chomp(P),chomp(B)}|Props]
+            end;
+        _ -> Props
     end,
-    Props.
+    read_baseforms(Fhd, Newprops, Cnt+1).
 
 take_args([{K,[_]}|_],_) when
     K=:=h ->
@@ -95,14 +102,16 @@ take_args([],Args) ->
 
 if_stopwords(Word) ->
     Stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your",
-    any(fun(X)-> X==Word end,tokens(Stopwords,",")).
+    any(fun(X)-> X=:=Word end,tokens(Stopwords,",")).
 
-chomp([H|T]) ->
-    case T of
-        "\n" -> [H];
+chomp(L) ->
+    Li = case reverse(L) of
+        [$\ |T] -> T;
+        [$\n|T] -> T;
         [] -> [];
-        _ -> [H | chomp(T)]
-    end.
+        R -> R
+    end,
+    reverse(Li).
 
 ngram_count([H|T],G,N) when length(G)<N ->
     ngram_count(T,[H|G],N);
@@ -159,8 +168,7 @@ save_pmap([],Fho) ->
 line_read(File) ->
     case file:read_line(File) of
         eof -> eof;
-        {ok,Data} ->
-            chomp(Data);
+        {ok,Data} -> chomp(Data);
         {error,Reason} ->
             io:format("Error ~p.~n",[Reason]),
             exit(badarith)
