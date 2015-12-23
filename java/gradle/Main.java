2dc4d2e78221a9ea878d3751308cde7c617b5127
nishidy <nishidyatutokyo@gmail.com>
2015-12-23 11:01:09 +0900

    Removed imports.

--- java/gradle/Main.java
+++ java/gradle/Main.java
@@ -1,15 +1,3 @@
-import java.io.*;
-import java.util.regex.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.concurrent.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-
 import com.github.nishidy.ParseWikipediaXML.*;
 
 public class Main {
--- java/gradle/src/main/java/ParseMain.java
+++ java/gradle/src/main/java/ParseMain.java
@@ -1,17 +1,5 @@
 package com.github.nishidy.ParseWikipediaXML;
 
-import java.io.*;
-import java.util.regex.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.concurrent.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-
 public class ParseMain {
 
     public static void main(String... args){
