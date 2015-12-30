2b3ff804a238ea3ae8b4a33485c897b8e40221af
nishidy <nishidyatutokyo@gmail.com>
2015-12-30 23:04:54 +0900

    Implemented streaming XML document to get raw text and extract text in DOM structure.

--- c/ParseWikipediaXML.c
+++ c/ParseWikipediaXML.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+#define LSIZE 65535
 
 typedef unsigned int ui;
 
 typedef struct {
     char term[64];
     int  freq;
-
 } Bofw;
 
 int getIndexOfTerm(Bofw *bofw, ui cnt_bofw, char *term){
@@ -26,6 +28,7 @@ void save_file(Bofw *bofw, ui cnt_bofw, FILE* fpo){
     }
     fprintf(fpo,"\n");
 }
+
 void disp_stdio(Bofw *bofw, ui cnt_bofw){
     for(ui i=0;i<cnt_bofw;i++){
         if(i>0)  printf(" ");
@@ -54,6 +57,104 @@ int sort_bofw(const void *a, const void *b){
             return -1;
         }
     }
+}
+
+void strcaterr(){
+    fprintf(stderr,"strcat does not return the valid pointer.\n");
+    exit(1);
+}
+
+char* getElementDomText(char *dom, char *tag){
+    char open_tag[32] = {0};
+    char close_tag[32] = {0};
+
+    char *text = (char*)malloc(sizeof(char)*LSIZE);
+    memset(text,0,sizeof(char)*LSIZE);
+
+    sprintf(open_tag,"<%s",tag);
+    sprintf(close_tag,"</%s",tag);
+
+    char *open_pos = strstr(strstr(dom, open_tag),">")+1;
+    char *close_pos= strstr(dom, close_tag)-1;
+
+    ui text_len = (close_pos-open_pos)/sizeof(char);
+
+    if(open_pos!=NULL && close_pos!=NULL){
+        strncpy(text, open_pos, text_len);
+    }else{
+        text = NULL;
+    }
+
+    return text;
+}
+
+char* getElementTextRaw(FILE* fp, char *tag){
+
+    char l[LSIZE] = {0};
+    char open_tag[32] = {0};
+    char close_tag[32] = {0};
+
+    sprintf(open_tag,"<%s",tag);
+    sprintf(close_tag,"</%s",tag);
+
+    char tag_flag = 0;
+    char *text = (char*)malloc(sizeof(char)*LSIZE);
+    memset(text,0,sizeof(char)*LSIZE);
+
+    while(fgets(l,LSIZE,fp)){
+        if(strstr(l,open_tag)!=NULL){
+            tag_flag = 1;
+        }
+        if(tag_flag == 1){
+            if(strcat(text,l)!=text) strcaterr();
+        }
+        if(strstr(l,close_tag)!=NULL){
+            if(strcat(text,l)!=text) strcaterr();
+            tag_flag = 2;
+            break;
+        }
+        if(strlen(text)%LSIZE > (LSIZE/2)){
+            text = (char*)realloc(text, sizeof(char) * LSIZE * (strlen(text)/LSIZE));
+        }
+    }
+
+    if(tag_flag==2){
+        return text;
+    }else{
+        return NULL;
+    }
+}
+
+char* getElementText(char *tag){
+    return "";
+}
+
+void toLower(char *term){
+    for(ui i=0;i<strlen(term);i++){
+        term[i] = tolower(term[i]);
+    }
+}
+
+int isValidTerm(char *term){
+    char *c;
+    if(strlen(term)<2) return 0;
+
+    for(ui i=0;i<strlen(term);i++){
+        if(i==0){
+            if(!( isalpha(term[i]) )) return 0;
+        }else if(i==strlen(term)-1){
+            if(!( isalnum(term[i]) ||
+                  term[i]=='\''    ||
+                  term[i]=='-'
+            )) return 0;
+        }else{
+            if(!( isalpha(term[i]) ||
+                  isdigit(term[i])
+            )) return 0;
+        }
+    }
+
+    return 1;
 }
 
 int main(int argc, char* argv[]){
@@ -95,26 +196,41 @@ int main(int argc, char* argv[]){
     if(strlen(outBofwFile)>0){
         fpo = fopen(outBofwFile,"w");
     }
-    char l[65535] = {0};
 
-    while(fgets(l,65535,fpi)){
-        Bofw bofw[65535] = {0};
+    char *page_raw;
+    char *text_raw;
+    while( (page_raw=getElementTextRaw(fpi,"page"))!=NULL ){
+
+        text_raw=getElementDomText(page_raw, "text");
+
         char *c;
+        c = strtok(text_raw, ".,;\n");
+
+        Bofw bofw[LSIZE] = {0};
         ui cnt_bofw = 0;
-        c = strtok(l, " .,;\n");
         while(c != NULL){
-            int idx_bofw = getIndexOfTerm(bofw, cnt_bofw, c);
-            if(idx_bofw>-1){
-                bofw[idx_bofw].freq++;
-            }else{
-                strncpy(bofw[cnt_bofw].term, c, strlen(c));
-                bofw[cnt_bofw].freq = 1;
-                cnt_bofw++;
+            if(isValidTerm(c)>0){
+                toLower(c);
+                int idx_bofw = getIndexOfTerm(bofw, cnt_bofw, c);
+                if(idx_bofw>-1){
+                    bofw[idx_bofw].freq++;
+                }else{
+                    strncpy(bofw[cnt_bofw].term, c, strlen(c));
+                    bofw[cnt_bofw].freq = 1;
+                    cnt_bofw++;
+                }
             }
             c=strtok(NULL," .,;\n");
         }
-        qsort((void *)bofw, cnt_bofw, sizeof(Bofw), sort_bofw);
-        save(bofw, cnt_bofw, fpo);
+
+        if(cnt_bofw>0){
+            qsort((void *)bofw, cnt_bofw, sizeof(Bofw), sort_bofw);
+            save(bofw, cnt_bofw, fpo);
+        }
+
+        free(text_raw);
+        free(page_raw);
+
     }
 
     fclose(fpi);
