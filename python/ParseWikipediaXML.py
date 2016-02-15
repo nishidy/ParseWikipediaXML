08f79439b1e3923a4ea42fee24a9bea6fcf73166
nishidy <nishidyatutokyo@gmail.com>
2016-02-15 23:09:34 +0900

    Enabled to specify separator.

--- python/ParseWikipediaXML.py
+++ python/ParseWikipediaXML.py
@@ -358,6 +358,13 @@ class EngParser(AbstParser):
 
         stopwords="a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
         self.stopwords=stopwords.split(",")
+        self.setRegexp(r" ", r"^[a-z][a-z0-9'-]*[a-z0-9]$")
+
+    def setRegexp(self, separator="", termexp=""):
+        if separator:
+            self.separator = separator
+        if termexp:
+            self.termexp = termexp
 
     @check_time
     def readDictionary(self):
@@ -403,10 +410,10 @@ class EngParser(AbstParser):
         totalNumOfWords = 0
         dictBofw=defaultdict(int) # python >= 2.5
 
-        for word in text.split():
+        for word in re.sub(self.separator," ",text).split():
             word = word.lower()
             if word in self.stopwords: continue
-            if re.search("^[a-z][a-z0-9'-]*[a-z0-9]$",word) is None: continue
+            if re.search(self.termexp,word) is None: continue
              # defaultdict initializes the fist value of a key
             dictBofw[self.dictMap.get(word,word)] += 1
             totalNumOfWords +=1
---
+++ python/main.py
@@ -0,0 +1,8 @@
+from ParseWikipediaXML import Parser
+import sys
+
+parser = Parser(sys.argv).new()
+parser.readDictionary()
+parser.setRegexp(separator=r"[.,;|\n]")
+parser.startParse()
+
