de4246099f396198c1ce2965d21b3c974c74ac9e
nishidy <nishidyatutokyo@gmail.com>
2016-05-13 01:05:45 +0900

    Enabled to save the result in file.

--- rust/parse_wikipedia_xml/src/main.rs
+++ rust/parse_wikipedia_xml/src/main.rs
@@ -4,14 +4,20 @@ use std::env;
 use std::collections::HashMap;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::Write;
 use std::fs::File;
 use regex::Regex;
 
-fn output_result(dicts: &HashMap<String, isize>) {
+fn output_result(dicts: &HashMap<String, isize>, fd: &mut File) {
+	let mut output = Vec::new();
 	for (k,v) in dicts.iter() {
-		print!("{} {} ",k,v)
+		output.push(k.clone());
+		output.push(v.to_string());
 	}
-	println!("")
+	match fd.write_fmt(format_args!("{}\n",output.join(" "))) {
+		Ok(_) => (),
+		Err(v) => panic!("{}",v)
+	}
 }
 
 fn update_dict(dicts: &mut HashMap<String, isize>, lines_str: &String) -> bool {
@@ -34,13 +40,19 @@ fn update_dict(dicts: &mut HashMap<String, isize>, lines_str: &String) -> bool {
 fn main() {
 
 	let args: Vec<String> = env::args().collect();
-	if args.len() != 2 {
+	if args.len() != 3 {
 		panic!("Not enough argument.");
 	}
 
 	let db_file = &args[1];
+	let save_file = &args[2];
 
-	let fd = match File::open(db_file) {
+	let fd_in = match File::open(db_file) {
+		Ok(v) => v,
+		Err(_) => return ()
+	};
+
+	let mut fd_out = match File::create(save_file) {
 		Ok(v) => v,
 		Err(_) => return ()
 	};
@@ -48,13 +60,13 @@ fn main() {
 	let mut dicts = HashMap::new();
 	let mut lines_str: String = "".to_string();
 
-	let buf = BufReader::new(&fd);
+	let buf = BufReader::new(&fd_in);
 	for line in buf.lines() {
 		let line_str = line.unwrap();
 		lines_str.push_str(&line_str);
 		if update_dict(&mut dicts,&lines_str) {
 			if dicts.len() > 0 {
-				output_result(&dicts)
+				output_result(&dicts,&mut fd_out)
 			}
 			dicts.clear();
 			lines_str.clear();
